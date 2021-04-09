7af7e5ec6593173de1e29e62214854216842140a
sfch1999 <sor100100@yahoo.com>
2021-04-09 18:38:05 +0430

    Tests tested

--- src/tests/PowerTest.java
+++ src/tests/PowerTest.java
@@ -1,29 +1,34 @@
 package tests;
+import main.implementations.multiply.RecursiveMult;
+import main.implementations.multiply.StandardMult;
+import main.implementations.power.RecursivePow;
+import main.implementations.power.StandardPow;
+import main.interfaces.Power;
 import org.junit.Test;
 import org.junit.Assert;
 
 public class PowerTest {
     @Test
     public void testPowerStandardRecursive() {
-        Power power = new PowerImplStandard(new MultImplRecursive());
+        Power power = new StandardPow(new RecursiveMult());
         Assert.assertEquals(81, power.power(3, 4), 0);
     }
 
     @Test
     public void testPowerRecursiveRecursive() {
-        Power power = new PowerImplRecursive(new MultImplRecursive());
+        Power power = new RecursivePow(new RecursiveMult());
         Assert.assertEquals(81, power.power(3, 4), 0);
     }
 
     @Test
     public void testPowerRecursiveStandard(){
-        Power power = new PowerImplRecursive(new MultImplStandard());
+        Power power = new RecursivePow(new StandardMult());
         Assert.assertEquals(81, power.power(3, 4), 0);
     }
 
     @Test
     public void testPowerStandardStandard(){
-        Power power = new PowerImplStandard(new MultImplStandard());
+        Power power = new StandardPow(new StandardMult());
         Assert.assertEquals(81, power.power(3, 4), 0);
     }
 }
